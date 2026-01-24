8827fec03cc61aea461c03cf65053e1c289c7860
currency-Simple <jamalkatab05@gmail.com>
2026-01-24 23:38:38 +0100

    Create fonts.js

---
+++ fonts/fonts.js
@@ -0,0 +1,40 @@
+// fonts/fonts.js
+const LOCAL_FONTS = [
+    { 
+        name: "Agu Display", 
+        family: "'Agu Display', display", 
+        demo: "نص تجريبي",
+        file: "agu-display.woff2", // سيحمل ديناميكياً
+        url: null
+    },
+    { 
+        name: "Oswald", 
+        family: "'Oswald', sans-serif", 
+        demo: "نص تجريبي",
+        file: "oswald-variable.woff2",
+        url: null
+    },
+    { 
+        name: "Pacifico", 
+        family: "'Pacifico', cursive", 
+        demo: "نص تجريبي",
+        file: "pacifico.woff2",
+        url: null
+    },
+    { 
+        name: "Reem Kufi", 
+        family: "'Reem Kufi', sans-serif", 
+        demo: "نص تجريبي",
+        file: "reem-kufi.woff2",
+        url: null
+    },
+    { 
+        name: "Playfair Display", 
+        family: "'Playfair Display', serif", 
+        demo: "نص تجريبي",
+        file: "playfair-display.woff2",
+        url: null
+    }
+];
+
+// يمكنك إضافة جميع الخطوط الأخرى بنفس الطريقة
